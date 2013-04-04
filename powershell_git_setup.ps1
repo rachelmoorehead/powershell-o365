9d53b3bbf953e1c90ad29e5fa05e9cb4bbaa9ee9
Rachel M <rmoorehe@uga.edu>
2013-04-04 12:01:27 -0400

    Added future usage

--- powershell_git_setup.ps1
+++ powershell_git_setup.ps1
@@ -10,8 +10,9 @@
 # 1. Install GitHub for Windows and msysgit
 # http://msysgit.github.com/
 
-# 2. Add git alias to PowerShell profile
+# 2. Add git alias and the msysgit/bin folder to your PATH variable to PowerShell profile
 New-Item -path alias:git -Value 'C:\msysgit\cmd\git.exe' #This is where mine installed.
+$env:path += ";C:\msysgit\bin" #This is where mine installed.
 
 # 3. Clone Posh-Git to your Repository from the web interface, button at the top
 # https://github.com/dahlbyk/posh-git
@@ -27,3 +28,18 @@ cd .\Documents\GitHub\posh-git
 # Future Usage
 # cd to GitHub directory
 git help # for available commands
+
+git status
+
+# For a new Repo, or to reinitialize an existing one
+git init
+git fetch origin
+
+# To upload files
+git add <name>
+
+# To commit files to stage
+git commit -m "description"
+
+# To update GitHub
+git push origin master
