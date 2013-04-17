e62c3f6fef333e7a5086d61ede3e324afc28697b
Rachel M <rmoorehe@uga.edu>
2013-04-17 11:08:25 -0400

    WIP - Initial Commit

---
+++ scripting_error_handling.ps1
@@ -0,0 +1,25 @@
+﻿<#
+
+    Various O365 Specific Error Handling
+
+#>
+
+
+### Checking for an Active Session ###
+
+$status = (Get-PSSession).State
+
+if($status -eq 'Opened'){ <# Do Something #> } else { <# Start a New Session #> }
+
+
+### Try - Catch Statements ###
+
+try{  <# Your Remote Call #>  }
+
+<# Specific Catch Statements #>
+
+catch [ParameterArgumentValidationErrorNullNotAllowed] { <# Log It #> }
+
+catch [System.Exception] { <# Log It #> }
+
+catch { <# Generic Catch - Log It #> }
